4848707fe7c6d4b222f9a4270afea89e9c5267f9
illbefront <v.matiakh@betinvest.com>
2025-02-05 23:48:41 +0200

    feat(partnership): add route

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ const PORT = 8000;
 
 const { homePageController } = require('./src/controllers/home');
 const { lotteriesPageController } = require('./src/controllers/lotteries');
+const { partnershipPageController } = require('./src/controllers/partnership');
 
 app.get('/home', homePageController);
 app.get('/lotteries', lotteriesPageController);
+app.get('/partnership', partnershipPageController);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
---
+++ src/controllers/partnership/index.js
@@ -0,0 +1,9 @@
+const data = require("../../../mock-data/partnership.json");
+
+const partnershipPageController = (req, res) => {
+    res.send(data);
+};
+
+module.exports = {
+    partnershipPageController
+}
